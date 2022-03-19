7f5929a73a90145c816fd49ced30e31a2c8aa964
staszewski <kamilstaszewski95@gmail.com>
2022-03-19 11:12:16 +0100

    Run shell function

--- rust_lambda_helpers.zsh
+++ rust_lambda_helpers.zsh
@@ -1,6 +1,8 @@
 RUST_TARGET="aarch64-unknown-linux-gnu" # corresponding with the above, set this to aarch64 or x86_64 -unknown-linux-gnu for ARM or x86 functions.
 
 zipRustLambda() {
-  echo "NANA DOING"
+  echo "NANA"
 	cp ./target/${RUST_TARGET}/release/rust_on_aws ./bootstrap && zip lambda.zip bootstrap && rm bootstrap
 }
+
+zipRustLambda
